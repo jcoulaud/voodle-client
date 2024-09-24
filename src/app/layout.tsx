57c8523f580742aead0ef3014e16939aef5cd10b
Julien Coulaud <j.coulaud@gmail.com>
2024-09-24 18:31:57 +0100

    fix(naming): replaced Yoloy by Voodle

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -6,7 +6,7 @@ import './styles/globals.css';
 const inter = Inter({ subsets: ['latin'] });
 
 export const metadata = {
-  title: 'Yoloy',
+  title: 'Voodle',
   description: 'Trading Made Easy',
 };
 
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -17,7 +17,7 @@ export default function Home() {
           />
         </div>
         <div className='mx-auto max-w-2xl text-center'>
-          <h1 className='text-4xl font-bold tracking-tight text-foreground sm:text-6xl'>YOLOY</h1>
+          <h1 className='text-4xl font-bold tracking-tight text-foreground sm:text-6xl'>Voodle</h1>
           <p className='mt-6 text-lg leading-8 text-muted-foreground'>Trading made easy</p>
           <div className='mt-10 flex items-center justify-center'>
             <Link href='/login' passHref>
